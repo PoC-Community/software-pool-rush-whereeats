7f22ab0b9c8d64ba406273b1660d122929ea8203
Thomas <thomas.heiles@epitech.eu>
2023-02-11 12:42:38 +0100

    feat: login and register page

---
+++ mobile_app/lib/components/googleButton.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
+
+class SignUpButtonGoogle extends StatelessWidget {
+  const SignUpButtonGoogle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle signUpGoogleBtnStyle = ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        minimumSize: const Size(70, 40),
+        maximumSize: const Size(70, 40),
+        backgroundColor: Colors.blue,
+        side: const BorderSide(color: Colors.grey, width: 2.0));
+
+    return SizedBox(
+      width: 300.0,
+      height: 50.0,
+      child: ElevatedButton(
+        onPressed: () {
+          print('Continue with Google Button Pressed');
+        },
+        style: signUpGoogleBtnStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Icon(
+              FontAwesome.google,
+              color: Colors.white,
+              size: 18.0,
+            ),
+            SizedBox(width: 8.0),
+            Text(
+              'Continue with Google',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
