1bf1c682a7ffdfef33f368ea4f84d0b0e8926e1b
Thomas <thomas.heiles@epitech.eu>
2023-02-11 12:04:01 +0100

    feat: home

--- mobile_app/lib/pages/home.dart
+++ mobile_app/lib/pages/home.dart
@@ -17,19 +17,24 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        color: Colors.black,
+        color: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "Spotify",
+              "WhereEats.",
               style: TextStyle(
                   fontSize: 44.0,
                   fontWeight: FontWeight.bold,
-                  color: Colors.green),
+                  color: Colors.blue),
             ),
-            SizedBox(height: 100.0),
+            SizedBox(height: 300.0),
             Container(
+              width: 200.0,
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
               child: ElevatedButton(
                 onPressed: () {
                   // Navigator.push(
@@ -39,7 +44,7 @@ class _HomePageState extends State<HomePage> {
                 },
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.black,
-                  backgroundColor: Colors.green,
+                  backgroundColor: Colors.blue,
                 ),
                 child: Text(
                   "Login",
@@ -53,7 +58,7 @@ class _HomePageState extends State<HomePage> {
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 foregroundColor: Colors.black,
-                backgroundColor: Colors.green,
+                backgroundColor: Colors.white,
               ),
               onPressed: () {
                 // Navigator.push(
